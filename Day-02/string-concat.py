63ad408d5baf3ab816b99d0f178f7871849eca92
Satheesh Kamadani <satheesh@Satheeshs-MacBook-Air.local>
2025-01-02 22:44:21 +0100

    Added string data type examples

---
+++ Day-02/string-concat.py
@@ -0,0 +1,6 @@
+# String concatination
+
+str1 = "Satheesh"
+str2 = "Kamadani"
+name = str1 + " " + str2
+print(name)
---
+++ Day-02/string-len.py
@@ -0,0 +1,4 @@
+# String length
+text = "Python is a awesome programming language"
+length = len(text)
+print("Length of the string:", length)
