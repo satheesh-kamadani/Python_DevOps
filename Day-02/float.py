dc0317f30e208cf64428fef530eea01c6b115c15
Satheesh Kamadani <satheesh@Satheeshs-MacBook-Air.local>
2025-01-04 11:43:40 +0100

    Added float and integer data type scripts

---
+++ Day-02/float.py
@@ -0,0 +1,21 @@
+# Float data type
+
+num1 = 9.2
+num2 = 5.3
+
+# Basic arthmetic
+result = num1 + num2
+print("Addition:", result)
+
+result1 = num1 - num2
+print("Substration:", result1)
+
+result2 = num1 * num2
+print("Multiplication:", result2)
+
+result3= num1 / num2
+print("Division:", result3)
+
+# Rounding
+result4 = round(3.14576967909)
+print("Rounded:", result4)
---
+++ Day-02/int.py
@@ -0,0 +1,31 @@
+# Integer data type
+
+num1 = 10
+num2 = 5
+
+# Basic arthmetic
+result = num1 + num2
+print("Addition:", result)
+
+result1 = num1 - num2
+print("Substration:", result1)
+
+result2 = num1 * num2
+print("Multiplication:", result2)
+
+result3= num1 / num2
+print("Division:", result3)
+
+# Integer division
+result4 = num1 // num2
+print("Integer division:", result4)
+
+# Modulus (Remainder)
+result5= num1 % num2
+print("Modulus (Remainder):", result5)
+
+# Absolute value
+result6 = abs(-7)
+print("Absolute value:", result6)
+
+
