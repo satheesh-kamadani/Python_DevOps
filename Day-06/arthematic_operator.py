a3545c096f3dabb0d3e8f2af2279216876452056
Kamadani <SKAMADAN@volvocars.com>
2025-01-09 12:31:09 +0100

    Added operators scripts

---
+++ Day-06/arthematic_operator.py
@@ -0,0 +1,14 @@
+# Arthematic operators
+
+a = 20
+b = 30
+
+sum = a + b
+sub = a - b
+mul = a * b
+div = a / b
+
+print(f"sum_result: {sum}")
+print(f"sub_result: {sub}")
+print(f"mul_result: {mul}")
+print(f"div_result: {div}")
---
+++ Day-06/logical_operator.py
@@ -0,0 +1,14 @@
+# Logical operator
+
+x = True
+y = False
+
+and_result = x and y
+or_result = x or y
+not_x = not x
+not_y = not y
+
+print(f"x AND y: {x} and {y} = {and_result}")
+print(f"x OR y: {x} or {y} = {or_result}")
+print(f"NOT x: not {x} = {not_x}")
+print(f"NOT y: not {y} = {not_y}")
